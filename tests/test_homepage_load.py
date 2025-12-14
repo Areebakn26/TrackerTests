e72de9a6ae391699fbb36a296bc83b9812945df1
Areebakn26 <areebaniazi26@gmail.com>
2025-12-14 20:36:55 +0500

    Update test_homepage_load.py

--- tests/test_homepage_load.py
+++ tests/test_homepage_load.py
@@ -4,7 +4,7 @@ from test_setup import get_driver
 def test_homepage_title():
     driver = get_driver()
     try:
-        driver.get("http://65.0.173.26:3000/")
+        driver.get("http://65.0.27.191:3000/")
         assert "Expense" in driver.title
     finally:
         driver.quit()
