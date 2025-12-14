3f9999cf6d45610725f8d101615d5151010d00ca
Areebakn26 <areebaniazi26@gmail.com>
2025-12-14 20:37:16 +0500

    Update login button test URL

--- tests/test_login_button.py
+++ tests/test_login_button.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_login_button_exists():
     driver = get_driver()
     try:
-        driver.get("http://52.66.246.182:3000/")
+        driver.get("http://65.0.27.191:3000/")
         wait = WebDriverWait(driver, 6)
         button = wait.until(EC.presence_of_element_located((By.ID, "login-btn")))
         assert button.is_displayed()
