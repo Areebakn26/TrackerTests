a963b9dbe69ae08d5e2071d305fd3ccaecfaf316
Areebakn26 <areebaniazi26@gmail.com>
2025-12-15 17:34:00 +0500

    Update test_login_invalid.py

--- tests/test_login_invalid.py
+++ tests/test_login_invalid.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_invalid_login():
     driver = get_driver()
     try:
-        driver.get("http://65.0.27.191:3000/login")
+        driver.get("http://43.204.115.124:3000/login")
         wait = WebDriverWait(driver, 6)
         wait.until(EC.presence_of_element_located((By.ID, "email")))
 
